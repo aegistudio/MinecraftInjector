89f7892aec57f5d45b7cf893fad493cb1c6cef4c
aegistudio <1910567646@qq.com>
2016-05-05 19:32:15 +0800

    Adding play out map sender.

--- src/net/aegistudio/mcinject/network/PacketManager.java
+++ src/net/aegistudio/mcinject/network/PacketManager.java
@@ -1,13 +1,17 @@
 package net.aegistudio.mcinject.network;
 
+import org.bukkit.map.MapView;
+
 import net.aegistudio.mcinject.MinecraftServer;
 import net.aegistudio.mcinject.world.BlockPosition;
 import net.aegistudio.mcinject.world.World;
 import net.aegistudio.reflect.clazz.AbstractClass;
 import net.aegistudio.reflect.clazz.SamePackageClass;
 import net.aegistudio.reflect.method.AbstractExecutor;
+import net.aegistudio.reflect.method.Invocable;
 import net.aegistudio.reflect.method.LengthedExecutor;
 import net.aegistudio.reflect.method.MatchedExecutor;
+import net.aegistudio.reflect.method.ThisExecutor;
 
 public class PacketManager {
 	private final MinecraftServer server;
@@ -23,6 +27,16 @@ public class PacketManager {
 		AbstractClass piUseEntity = new SamePackageClass(server.getMinecraftServerClass(), "PacketPlayInUseEntity");
 		this.playInUseEntity = new LengthedExecutor(piUseEntity.constructor(), 0);
 		this.playInUseEntity_entityId = new MatchedExecutor(piUseEntity.field(), int.class);
+		
+		AbstractClass poMap = new SamePackageClass(server.getMinecraftServerClass(), "PlayOutMap");
+		for(Invocable constructor : poMap.constructor())
+			for(PacketPoMapHandler.EnumHandler handler : PacketPoMapHandler.EnumHandler.values())
+				if(handler.paramCount == constructor.getParameterList().length) {
+					this.playOutMap = new ThisExecutor(constructor);
+					this.playOutMapHandler = handler;
+					break;
+				}
+		if(this.playOutMap == null) throw new NoSuchMethodException();
 	}
 	
 	AbstractExecutor playOutBlockChange;
@@ -34,6 +48,15 @@ public class PacketManager {
 		World worldInstance = server.getWorldManager().getHandle(world);
 		BlockPosition position = server.getWorldManager().createBlockPosition(x, y, z);
 		return this.playOutBlockChange(worldInstance, position);
+	}
+	
+	AbstractExecutor playOutMap;
+	PacketPoMapHandler.EnumHandler playOutMapHandler;
+	public Packet playOutMap(MapView mapView, byte[] raster, int columnOffset,
+			int rowOffset, int columnLength, int rowLength) throws Exception {
+		Object packet = playOutMapHandler.handler.playOutMap(playOutMap, mapView, raster, 
+				columnOffset, rowOffset, columnLength, rowLength);
+		return new Packet(packet, true);
 	}
 	
 	AbstractExecutor playOutOpenSignEditor;
