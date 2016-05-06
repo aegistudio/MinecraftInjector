e2913d767d18f45bdb041433d171c1a8061e0365
aegistudio <1910567646@qq.com>
2016-05-06 21:32:31 +0800

    Update

--- src/net/aegistudio/mcinject/tileentity/CommandBlockListenerAbstract.java
+++ src/net/aegistudio/mcinject/tileentity/CommandBlockListenerAbstract.java
@@ -11,9 +11,15 @@ import net.aegistudio.reflect.method.NamedExecutor;
 public class CommandBlockListenerAbstract extends Instance<CommandBlockListenerAbstract.Class> {
 	public static class Class extends SamePackageClass {
 		AbstractExecutor sender;
+		AbstractExecutor executeCommand;
+		AbstractExecutor getName, setName;
 		public Class(MinecraftServer server) throws Exception {
 			super(server.getMinecraftServerClass(), "CommandBlockListenerAbstract");
 			this.sender = new NamedExecutor(field(), "sender");
+			this.executeCommand = new NamedExecutor(method(), "executeCommand");
+			
+			this.getName = new NamedExecutor(method(), "getName");
+			this.setName = new NamedExecutor(method(), "setName");
 		}
 	}
 	
@@ -27,5 +33,17 @@ public class CommandBlockListenerAbstract extends Instance<CommandBlockListenerA
 	
 	public void setSender(CommandSender sender) {
 		clazz.sender.invoke(thiz, sender);
+	}
+	
+	public int executeCommand(CommandSender another, String command) {
+		return (int)clazz.executeCommand.invoke(null, thiz, another, command);
+	}
+	
+	public String getName() {
+		return (String)clazz.getName.invoke(thiz);
+	}
+	
+	public void setName(String name) {
+		clazz.setName.invoke(thiz, name);
 	}
 }
