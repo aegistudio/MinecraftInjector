723e46c32ab300d1add78701ebcf0951f0d1bff9
aegistudio <1910567646@qq.com>
2016-05-06 20:23:09 +0800

    Update.

--- src/net/aegistudio/mcinject/tileentity/TileEntityCommand.java
+++ src/net/aegistudio/mcinject/tileentity/TileEntityCommand.java
@@ -1,5 +1,37 @@
 package net.aegistudio.mcinject.tileentity;
 
-public class TileEntityCommand {
+import net.aegistudio.mcinject.MinecraftServer;
+import net.aegistudio.reflect.clazz.SamePackageClass;
+import net.aegistudio.reflect.method.AbstractExecutor;
+import net.aegistudio.reflect.method.LengthedExecutor;
+import net.aegistudio.reflect.method.NamedExecutor;
 
+public class TileEntityCommand extends TileEntity<TileEntityCommand.Class>{
+	public static class Class extends TileEntity.SubClass {
+		AbstractExecutor constructor;
+		AbstractExecutor getCommandBlock;
+		
+		public Class(MinecraftServer server) throws Exception {
+			super(server, new SamePackageClass(server.getMinecraftServerClass(), "TileEntityCommand"));
+			constructor = new LengthedExecutor(constructor(), 0);
+			getCommandBlock = new NamedExecutor(method(), "getCommandBlock");
+		}
+		
+		public Object newInstance() {
+			return constructor.invoke(null);
+		}
+		
+		public Object getCommandBlock(Object instance) {
+			return getCommandBlock.invoke(instance);
+		}
+	}
+	
+	public TileEntityCommand(Class clazz, Object instance) {
+		super(clazz, instance);
+	}
+	
+	public TileEntityCommand(MinecraftServer server) {
+		super(server.getTileEntityManager().tileEntityCommand.getClazz(), 
+				server.getTileEntityManager().tileEntityCommand.getClazz().newInstance());
+	}
 }
--- src/net/aegistudio/mcinject/tileentity/TileEntityManager.java
+++ src/net/aegistudio/mcinject/tileentity/TileEntityManager.java
@@ -6,8 +6,11 @@ import net.aegistudio.mcinject.ProxiedClass;
 public class TileEntityManager {
 	ProxiedClass<TileEntity.Class> tileEntity;
 	ProxiedClass<TileEntitySign.Class> tileEntitySign;
+	ProxiedClass<TileEntityCommand.Class> tileEntityCommand;
+	
 	public TileEntityManager(MinecraftServer server) {
 		tileEntity = new ProxiedClass<TileEntity.Class>(server, s -> new TileEntitySign.SuperClass(s));
 		tileEntitySign = new ProxiedClass<TileEntitySign.Class>(server, s -> new TileEntitySign.Class(s));
+		tileEntityCommand = new ProxiedClass<TileEntityCommand.Class>(server, s -> new TileEntityCommand.Class(s));
 	}
 }
